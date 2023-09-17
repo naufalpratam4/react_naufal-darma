7d0751fcd47b950e79a57cbc59c602998388ff04
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-09-17 22:33:58 +0700

    delete file

--- 14_React-Hook/Praktikum/react-hook/src/main.jsx
+++ 14_React-Hook/Praktikum/react-hook/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 {
   /* The following line can be included in your src/index.js or App.js file */
 }
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
