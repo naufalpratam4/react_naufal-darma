d1bcb9fe3ecc585d6433ad7e487b3f81e83e35ae
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-02 13:55:30 +0700

    tes

--- 18_React-Testing/Praktikum/react-testing/src/pages/CreateProduct.test.js
+++ 18_React-Testing/Praktikum/react-testing/src/pages/CreateProduct.test.js
@@ -1,32 +1,22 @@
+// CreateProduct.test.js
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CreateProduct from "./CreateProduct";
 
-describe("CreateProduct Component", () => {
-  test("renders component properly", () => {
-    render(<CreateProduct />);
+test("memastikan bahwa form input Product Name dapat menerima input teks dan menampilkannya di halaman", () => {
+  // Render CreateProduct component
+  render(<CreateProduct />);
 
-    expect(screen.getByText("Create Product")).toBeInTheDocument();
-    expect(screen.getByLabelText("Product Name")).toBeInTheDocument();
-    // Add more assertions as needed based on your UI structure
-  });
+  // Temukan input Product Name berdasarkan label atau atribut lain yang sesuai
+  const productNameInput = screen.getByLabelText("Product Name");
 
-  test("handles form submission", () => {
-    render(<CreateProduct />);
+  // Masukkan teks ke dalam input Product Name
+  fireEvent.change(productNameInput, { target: { value: "Nama Produk" } });
 
-    // Simulate user input
-    fireEvent.change(screen.getByLabelText("Product Name"), {
-      target: { value: "Test Product" },
-    });
-    fireEvent.change(screen.getByLabelText("Product Category"), {
-      target: { value: "Test Category" },
-    });
-    // Add more simulated user input as needed
+  // Pastikan bahwa nilai input telah diperbarui sesuai dengan yang dimasukkan
+  expect(productNameInput.value).toBe("Nama Produk");
 
-    // Trigger form submission
-    fireEvent.submit(screen.getByTestId("form"));
-
-    // Add your assertions based on the expected behavior after form submission
-    // For example, you can check if the state is updated or if an API request is made
-  });
+  // Atau Anda juga dapat memastikan bahwa nilai telah muncul di halaman
+  const displayedProductName = screen.getByText("Nama Produk");
+  expect(displayedProductName).toBeInTheDocument();
 });
