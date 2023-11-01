93e559308d658a67a5ac0a1ba59dab776d6a684e
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-11-01 22:40:16 +0700

    update

--- AuthenticationReact/authreact/src/App.jsx
+++ AuthenticationReact/authreact/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import SignUp from "./components/auth/SignUp";
 import Login from "./components/auth/SignIn";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      {
+        <Routes>
+          <Route path="/" exact element={<>Haloo</>} />
+        </Routes>
+      }
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
